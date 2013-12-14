75b01c81dae31775238ed7e34846da6fe95a0df4
eugene <sejoker@gmail.com>
2013-12-14 15:46:23 +0200

    draft frontend added with regular screen support

---
+++ HW3/frontend/README.md
@@ -0,0 +1,4 @@
+Mockups:
+Regular screen: https://moqups.com/sudodoki/KljcOP8V/p:af3877c94
+Wide screen: https://moqups.com/sudodoki/KljcOP8V/p:a7126e63c
+Mobile: https://moqups.com/sudodoki/KljcOP8V/p:a8bf60b4a
--- HW4/querySelector.js
+++ HW4/querySelector.js
@@ -1,26 +1,71 @@
-var $ = function(selector){
-	var result = document.querySelectorAll(selector),
+// Your code goes here
+var my_$ = function(selector){
+	var domElements = document.querySelectorAll(selector),
 		styleFn = function(value, property){
-		var valueInt = parseInt(value, 10);
-		if (!isFinite(valueInt))
-			return;
 
-		[].forEach.call(result, function(div, idx, array){
-			div.style[property] = value + 'px';
+    var values = [];
+		[].forEach.call(domElements, function(element, idx, array){
+		  if (value){
+		    element.style[property] = value;  
+		  }
+			
+     values.push(element.style[property] == '' 
+        ? '0px' 
+        : element.style[property]);
 		});
+		
+		return values;
 	};
 
-	result.width = function(value){
-		styleFn.call(result, value, 'width');
+	domElements.width = function(size){
+	  return styleFn.call(domElements, size, 'width');
 	};
 
-	result.height = function(value){
-		styleFn.call(result, value, 'height');
+	domElements.height = function(size){
+		return styleFn.call(domElements, size, 'height');
 	};
-
-	return result;
+	
+  var applyCssSet = function(style){
+    for(var property in style){
+      if (style.hasOwnProperty(property)){
+        styleFn.call(domElements, style[property], property)
+      }
+      
+      return domElements;
+    }  
+  }
+  
+	var applyCssSingle = function(property, value){
+    styleFn.call(domElements, value, property);
+    return domElements;
+  }
+  
+  domElements.css = function(){
+    var args = arguments;
+    switch(arguments.length){
+      case 1: 
+          applyCssSet.apply(this, arguments);
+          break;
+        
+      case 2: 
+        applyCssSingle.apply(this, arguments);
+        break;
+      
+      case 3: 
+        //applyCssSingle(arguments[0], arguments[1])
+        
+        setTimeout( function(){ applyCssSingle(args[0], args[1]) }, args[2]);
+        //applyCssSingle.apply(this, arguments);
+        //return new Error('2323');
+        break;
+    }
+    
+    return domElements;
+  }
+	return domElements;
 };
 
+/*
 var $div = $('div');
 
 console.log( $div ); // 2 divs
@@ -29,4 +74,6 @@ var $red = $('.red');
 console.log( $red ); // 1 div
 
 $red.width('50');
-$red.height('100');
+*/
+my_$('.red').css('height', '500px', 1000)
+//$red.height('100');
